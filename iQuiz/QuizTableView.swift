ecee439cfa271b4b29c833b6abac24cb747338b0
yyfan <yyfan1994@gmail.com>
2017-04-28 13:35:02 -0700

    create tableview and tool bar

---
+++ iQuiz/QuizTableView.swift
@@ -0,0 +1,57 @@
+//
+//  QuizTableView.swift
+//  iQuiz
+//
+//  Created by iGuest on 4/28/17.
+//  Copyright © 2017 yyfan. All rights reserved.
+//
+
+import UIKit
+
+class QuizTableView: UITableView, UITableViewDelegate, UITableViewDataSource {
+    
+    var quizzes: [String]? = ["Mathematics", "Marvel Super Heroes", "Science"]
+    
+    // Data Source
+    //
+    @available(iOS 2.0, *)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return quizzes!.count
+    }
+    
+    @available(iOS 2.0, *)
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let element = quizzes?[indexPath.row]
+        let cellIdentifier = "ReuseCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) ?? UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
+        cell.textLabel?.text = element
+        
+        return cell
+    }
+
+    // Delegate
+    //
+    
+    
+    
+    
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        self.dataSource = self
+    }
+    
+    
+    
+    
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

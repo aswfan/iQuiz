5af0a60461619d6aabab77ba4d72810d23c781ad
yyfan <yyfan1994@gmail.com>
2017-05-13 15:40:54 -0700

    store data locally

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -16,23 +16,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     //
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.cellForRow(at: indexPath)?.setSelected(false, animated: true)
-        self.questions.removeAll()
-        self.answers.removeAll()
-        DispatchQueue.global(qos: .userInitiated).async {
-            self.fetchDataFromServer("https://tednewardsandbox.site44.com/questions.json", with: (self.titles?[indexPath.row])! )
-//            DispatchQueue.main.async {
-//                self.switchView()
-//            }
-        }
+        self.switchView(title: titles?[indexPath.row])
+        
         
     }
     
-    func switchView() {
+    func switchView(title: String?) {
+        if title == nil {
+            print("Something went wrong!")
+            return
+        }
+        print("title: \(title!)")
         let sb = UIStoryboard(name: "Main", bundle: nil)
         let vc: QuestionViewController = (sb.instantiateViewController(withIdentifier: "question") as? QuestionViewController)!
         
-        vc.questions = self.questions
-        vc.answers = self.answers
+        vc.questions = self.questions[title!]
+        vc.answers = self.answers[title!]
         
         self.present(vc, animated: true, completion: nil)
     }
@@ -43,13 +42,17 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.present(alert, animated: true, completion: nil)
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            self.fetchDataFromServer("https://tednewardsandbox.site44.com/questions.json" )
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        quizTable.dataSource = self
-        quizTable.delegate = self
         
-        quizTable.tableFooterView = UIView()
+        
         
     }
 
@@ -63,8 +66,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     var imgs: [UIImage]? = [#imageLiteral(resourceName: "Math"), #imageLiteral(resourceName: "Marvel"), #imageLiteral(resourceName: "Science")]
     var dess: [String]? = ["I love Math", "Batman is super cool", "Science makes world better"]
     
-    var questions = [[[String]]]()
-    var answers = [Int]()
+    var questions = [String: [[[String]]]]()
+    var answers = [String: [Int]]()
     
     
     // Data Source
@@ -80,64 +83,74 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         let cellIdentifier = "myTableCell"
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier) as? myTableViewCell ?? myTableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
         cell.title?.text = titles?[row]
-        cell.img?.image = imgs?[row]
+        cell.img?.image = imgs?[row % 3] // repeatedly using limited number of icons
         cell.des?.text = dess?[row]
         return cell
     }
 
     // fetch data from server
-    func fetchDataFromServer(_ serverURL: String, with: String) {
+    func fetchDataFromServer(_ serverURL: String) {
         let url = URL(string: serverURL)
         URLSession.shared.dataTask(with:url!) { (data, response, error) in
             if error != nil {
                 print(error!)
             } else {
+                DispatchQueue.main.async(execute: {
+                    self.titles?.removeAll()
+                    self.dess?.removeAll()
+                })
                 do {
                     let json = try JSONSerialization.jsonObject(with: data!) as? [[String: Any]]
                     for dict in json! {
-                        if let title = dict["title"] as? String {
-                            if title.range(of: with) == nil {
-                                continue
-                            }
-                            if let ques = dict["questions"] as? [[String: Any]] {
-                                for item in ques {
-                                    var strs = [[String]]()
-                                    var str = [String]()
-                                    if let text = item["text"] as? String {
-                                        str.append(text)
-                                    }
-                                    if str.count != 0 {
-                                        strs.append(str)
-                                    }
-                                    else {
-                                        strs.append([" "])
-                                    }
-                                    if let ans = item["answer"] as? String {
-                                        DispatchQueue.main.async(execute: {
-                                            self.answers.append(Int(ans)!)
-                                        })
-                                    }
-                                    if let anss = item["answers"] as? [String] {
-                                        strs.append(anss)
-                                    }
+                        let title = dict["title"] as! String
+                        DispatchQueue.main.async {
+                            self.titles?.append(title)
+                            self.dess?.append(dict["desc"] as! String)
+                            self.questions[title] = [[[String]]]()
+                            self.answers[title] = [Int]()
+                        }
+                        
+                        if let ques = dict["questions"] as? [[String: Any]] {
+                            for item in ques {
+                                var strs = [[String]]()
+                                var str = [String]()
+                                if let text = item["text"] as? String {
+                                    str.append(text)
+                                }
+                                if str.count != 0 {
+                                    strs.append(str)
+                                }
+                                else {
+                                    strs.append([" "])
+                                }
+                                if let ans = item["answer"] as? String {
                                     DispatchQueue.main.async(execute: {
-                                        self.questions.append(strs)
+                                        self.answers[title]?.append(Int(ans)!)
                                     })
                                 }
-                                
+                                if let anss = item["answers"] as? [String] {
+                                    strs.append(anss)
+                                }
+                                DispatchQueue.main.async(execute: {
+                                    self.questions[title]?.append(strs)
+                                })
                             }
+                            
                         }
                         
                     }
                 } catch let error as NSError {
                     print(error)
                 }
-                DispatchQueue.main.async(execute: {
-                    self.switchView()
-                })
+            }
+            DispatchQueue.main.async {
+                self.quizTable.dataSource = self
+                self.quizTable.delegate = self
+                
+                self.quizTable.tableFooterView = UIView()
             }
             
-            }.resume()
+        }.resume()
         
     }
 
